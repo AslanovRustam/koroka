4c2c3ed965553368a45f21fcecd2787d1e21500c
Aslanov Rustam <aslanovrg@gmail.com>
2021-05-23 14:25:33 +0300

    upd css and fix responsive layout

--- src/components/score/score.js
+++ src/components/score/score.js
@@ -8,10 +8,11 @@ import { ReactComponent as Video } from "../../images/Vectorvideo.svg";
 
 export default function Score() {
   return (
-    <div>
-      <div>
+    <div className={s.scoreContainer}>
+      <div className={s.scoreSection}>
         <div className={s.scoreCard}>
           <div className={s.userProgress}>
+            <div className={s.progressBox}></div>
             <span className={s.progress}>Progress 12%</span>
           </div>
           <img className={s.avatarImg} src={Alexa} />
@@ -22,16 +23,111 @@ export default function Score() {
             Take exam in Mathematics at 100 points
           </h2>
           <ul className={s.scoreInformation}>
-            <li className={s.scoreTitleItem} />
-            <Student className={s.icon} />
-            <span className={s.scoreTitle}>12 lessons</span>
-            <li className={s.scoreTitleItem} />
-            <Pen className={s.icon} />
-            <span className={s.scoreTitle}>12 tasks</span>
-            <li className={s.scoreTitleItem} />
-            <Video className={s.icon} />
-            <span className={s.scoreTitle}>30 minutes</span>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Student className={s.icon} />
+                <span className={s.scoreTitle}>12 lessons</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Pen className={s.icon} />
+                <span className={s.scoreTitle}>12 tasks</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Video className={s.icon} />
+                <span className={s.scoreTitle}>30 minutes</span>
+              </div>
+            </li>
           </ul>
+          <div className={s.buttonContainer}>
+            <button className={s.scoreButton} type="button">
+              Continue
+            </button>
+            <button className={s.moreButton} type="button">
+              More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={s.scoreSection}>
+        <div className={s.scoreCard}>
+          <div className={s.userProgressKathryn}>
+            <div className={s.progressBox}></div>
+            <span className={s.progress}>Progress 12%</span>
+          </div>
+          <img className={s.avatarImg} src={Kathryn} />
+          <span className={s.avatarName}>Kathryn Nguyen</span>
+        </div>
+        <div className={s.informSection}>
+          <h2 className={s.userLabel}>
+            How buy new clothes without money in USA
+          </h2>
+          <ul className={s.scoreInformation}>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Student className={s.icon} />
+                <span className={s.scoreTitle}>12 lessons</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Pen className={s.icon} />
+                <span className={s.scoreTitle}>12 tasks</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Video className={s.icon} />
+                <span className={s.scoreTitle}>30 minutes</span>
+              </div>
+            </li>
+          </ul>
+          <div className={s.buttonContainer}>
+            <button className={s.scoreButton} type="button">
+              Continue
+            </button>
+            <button className={s.moreButton} type="button">
+              More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={s.scoreSection}>
+        <div className={s.scoreCard}>
+          <div className={s.userProgressDiane}>
+            <div className={s.progressBox}></div>
+            <span className={s.progress}>Progress 12%</span>
+          </div>
+          <img className={s.avatarImg} src={Diane} />
+          <span className={s.avatarName}>Diane Miles</span>
+        </div>
+        <div className={s.informSection}>
+          <h2 className={s.userLabel}>TWhy people hate me?</h2>
+          <ul className={s.scoreInformation}>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Student className={s.icon} />
+                <span className={s.scoreTitle}>12 lessons</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Pen className={s.icon} />
+                <span className={s.scoreTitle}>12 tasks</span>
+              </div>
+            </li>
+            <li className={s.scoreListItem}>
+              <div className={s.scoreTitleItem}>
+                <Video className={s.icon} />
+                <span className={s.scoreTitle}>30 minutes</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className={s.buttonContainer}>
           <button className={s.scoreButton} type="button">
             Continue
           </button>
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -8,10 +8,12 @@ export default function Search() {
     <div>
       <div className={s.searchContainer}>
         <h1 className={s.searchHeader}>Course catalog</h1>
-        <span className={s.searchExplain}>
-          Each course contains video lectures, tasks, and text materials. All
-          courses viewed by you are displayed in your personal account
-        </span>
+        <div className={s.searchExplain}>
+          <span>
+            Each course contains video lectures, tasks, and text materials. All
+            courses viewed by you are displayed in your personal account
+          </span>
+        </div>
       </div>
       <div>
         <form className={s.formContainer}>
@@ -20,10 +22,10 @@ export default function Search() {
             <input className={s.inputStyles} placeholder="Search" />
           </label>
           <label className={s.labelCountry}>
-            <input
-              className={s.inputStylesCountry}
-              placeholder="Your country"
-            ></input>
+            {/* <input className={s.inputStylesCountry} placeholder=""></input> */}
+            <input className={s.inputStylesPlaceholder} placeholder=""></input>
+            <span className={s.inputStylesCountry}>Your country</span>
+            <span className={s.inputStylesUSA}>USA</span>
             <Union className={s.iconUnion} />
             <Check className={s.iconCheck} />
           </label>
